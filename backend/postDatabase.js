f80a84d2ec7426c92720eb145b4c8abfcbb1f187
bipins867 <bipins867@gmail.com>
2025-02-24 16:47:27 +0530

    Just Checking ....

--- backend/postDatabase.js
+++ backend/postDatabase.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-
+console.log(process.env.SUPABASE_DATABASE_URL);
 const sequelize = new Sequelize(process.env.SUPABASE_DATABASE_URL, {
   dialect: "postgres", // Use PostgreSQL
   dialectOptions: {
