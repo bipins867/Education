e9f54652fd9011dd73da8e8890cd9f72e38424fc
bipins867 <bipins867@gmail.com>
2025-02-26 20:04:39 +0530

    updated....

--- backend/importantInfo.js
+++ backend/importantInfo.js
@@ -3,7 +3,7 @@ const supabase = require("./supabaseClient");
 
 const sequelize = require("./database"); // Adjust the path to your database configuration
 //const sequelize = require("./postDatabase"); // Adjust the path to your database configuration
-console.log(supabase)
+
 exports.baseDir=__dirname;
 exports.sequelize=sequelize;
 exports.supabase=supabase
